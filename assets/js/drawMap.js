fa8990ce41158bb5a1bf32554f39f2b2f2a5f4ee
enginbyram <enginbayram.eru@gmail.com>
2025-03-11 22:58:31 +0300

    map page made, osm file regenerated

---
+++ assets/js/drawMap.js
@@ -0,0 +1,7 @@
+
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
