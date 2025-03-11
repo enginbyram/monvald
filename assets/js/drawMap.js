0d2cfa555514f101463d8a0eaf5bfa7c78812d2b
enginbyram <enginbayram.eru@gmail.com>
2025-03-12 00:18:56 +0300

    map is suicidal but it works

--- assets/js/drawMap.js
+++ assets/js/drawMap.js
@@ -1,7 +1,31 @@
+---
 
+---
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([9.3361060367590589, 39.714780594855114], 5);
 
-L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('{{site.baseurl}}/tiles/{z}/{x}/{y}.png', {
+    minZoom: 5,
+    maxZoom: 9,
+    attribution: "&copy " + "Monvald by Engin Bayram"
 }).addTo(map);
+
+L.control.scale().addTo(map);
+
+/* ZOOM VIEWER */
+const ZoomViewer = L.Control.extend({
+    onAdd() {
+        const container = L.DomUtil.create('div');
+        const gauge = L.DomUtil.create('div');
+        container.style.width = '200px';
+        container.style.background = 'rgba(255,255,255,0.5)';
+        container.style.textAlign = 'left';
+        map.on('zoomstart zoom zoomend', (ev) => {
+            gauge.innerHTML = `Zoom level: ${map.getZoom()}`;
+        });
+        container.appendChild(gauge);
+        return container;
+    }
+});
+
+const zoomViewerControl = (new ZoomViewer()).addTo(map);
