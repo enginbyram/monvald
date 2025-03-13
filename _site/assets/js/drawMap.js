0012f971b3cd5daf0c1f801f45172d52b22f9df0
enginbyram <enginbayram.eru@gmail.com>
2025-03-13 23:17:38 +0300

    finally with real _site folder

---
+++ _site/assets/js/drawMap.js
@@ -0,0 +1,27 @@
+var map = L.map('map').setView([9.3361060367590589, 39.714780594855114], 5);
+
+L.tileLayer('/tiles/{z}/{x}/{y}.png', {
+    minZoom: 5,
+    maxZoom: 9,
+    attribution: "&copy " + "Monvald by Engin Bayram"
+}).addTo(map);
+
+L.control.scale().addTo(map);
+
+/* ZOOM VIEWER */
+const ZoomViewer = L.Control.extend({
+    onAdd() {
+        const container = L.DomUtil.create('div');
+        const gauge = L.DomUtil.create('div');
+        container.style.width = '200px';
+        container.style.background = 'rgba(255,255,255,0.5)';
+        container.style.textAlign = 'left';
+        map.on('zoomstart zoom zoomend', (ev) => {
+            gauge.innerHTML = `Zoom level: ${map.getZoom()}`;
+        });
+        container.appendChild(gauge);
+        return container;
+    }
+});
+
+const zoomViewerControl = (new ZoomViewer()).addTo(map);
